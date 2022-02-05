1a0cb7a63213b31de18c1f7f5bd62da26e2f425b
Albin <albinronnkvist@protonmail.com>
2022-02-05 21:27:18 +0100

    Minor changes beacuse of deployment

--- server/server.js
+++ server/server.js
@@ -7,19 +7,13 @@ const cors = require('cors')
 const mongoSanitize = require('express-mongo-sanitize');
 
 // Connect to DB
-mongoose.connect(process.env.DATABASE_URL_LOCAL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL_PRODUCTION, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to DB'))
 
 app.use(express.json())
-app.use(
-  cors(
-    {
-      origin: "*"
-    }
-  )
-)
+app.use(cors())
 app.use(mongoSanitize())
 
 
@@ -37,4 +31,4 @@ app.get("*", (req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' })
 })
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 5000)
