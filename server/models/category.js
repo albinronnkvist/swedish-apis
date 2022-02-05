f6a29242a60dc78dceabf3ed1c329b05f4bb54e8
Albin <albinronnkvist@protonmail.com>
2022-02-05 10:55:21 +0100

    Changed maxlength of category title

--- server/models/category.js
+++ server/models/category.js
@@ -4,7 +4,7 @@ const categorySchema = new mongoose.Schema({
   title: {
     type: String,
     minlength: [1, "Title too short, minimum 1 character"],
-    maxlength: [100, "Title too long, maximum 100 characters"],
+    maxlength: [50, "Title too long, maximum 50 characters"],
     required: [true, "Title required"]
   }
 })
